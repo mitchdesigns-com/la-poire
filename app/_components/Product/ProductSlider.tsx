bd670c40df045014429395dd5f303aa9d81d7e75
Ezzat Makar <ezzatmalak93icloud.com@Ezzats-Mac-mini.local>
2024-05-15 14:05:46 +0300

    fix order issue

--- app/_components/Product/ProductSlider.tsx
+++ app/_components/Product/ProductSlider.tsx
@@ -5,17 +5,24 @@ import ProductWidget from "./ProductWidget";
 
 export default function ProductSlider({ data }: any) {
   const list = data?.products.data;
+
+  // Sort the list by orderNumber
+  const sortedList = Array.isArray(list)
+    ? list.sort(
+        (a: any, b: any) => a.attributes.orderNumber - b.attributes.orderNumber
+      )
+    : [];
+
   return (
-      <SlickMultipleItems ArrowsPositionRight nopadding>
-        {Array.isArray(list) &&
-          list.map((item: any, index: any) => (
-            <div
-              key={index}
-              className="inline-flex flex-shrink-0 flex-grow-0 md:w-[260px] w-[calc(100vw-70px)]"
-            >
-              <ProductWidget data={item.attributes} />
-            </div>
-          ))}
-      </SlickMultipleItems>
+    <SlickMultipleItems ArrowsPositionRight nopadding>
+      {sortedList.map((item: any, index: any) => (
+        <div
+          key={index}
+          className="inline-flex flex-shrink-0 flex-grow-0 md:w-[260px] w-[calc(100vw-70px)]"
+        >
+          <ProductWidget data={item.attributes} />
+        </div>
+      ))}
+    </SlickMultipleItems>
   );
 }

8e0e791dcbc3b7346af3e579efc38c086b85460e
Ezzat Makar <ezzatmalak93icloud.com@Ezzats-Mac-mini.local>
2024-05-15 14:12:45 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -9,25 +9,25 @@ module.exports = withNextIntl({
   reactStrictMode: true,
   swcMinify: true,
   images: {
-
     remotePatterns: [
       {
         protocol: "https",
         hostname: process.env.NEXT_PUBLIC_IMAGES_DOMAIN,
       },
-    ],  },
-  async headers() {
-    return [
-      {
-        source: "/:all*(svg|jpg|png|webp)",
-        locale: false,
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=9999999999, stale-while-revalidate",
-          }
-        ],
-      },
-    ];
+    ],
   },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/:all*(svg|jpg|png|webp)",
+  //       locale: false,
+  //       headers: [
+  //         {
+  //           key: "Cache-Control",
+  //           value: "public, max-age=9999999999, stale-while-revalidate",
+  //         }
+  //       ],
+  //     },
+  //   ];
+  // },
 });

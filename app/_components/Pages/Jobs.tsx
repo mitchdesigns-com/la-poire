b939df0b0eeb7257b2d6d2243d5d6255bec6f11f
BasmaElsisy <basma.el.sisy@outlook.com>
2024-05-27 12:22:55 +0300

    updates kareem

--- app/_components/Pages/Jobs.tsx
+++ app/_components/Pages/Jobs.tsx
@@ -11,7 +11,7 @@ import { fetchingAllJobs } from "@/app/api/fetcher";
 export default function Jobs({ data }: any) {
   const mediaUrl = process.env.NEXT_PUBLIC_API_URL;
   // console.log(mediaUrl);
-  
+
   const [dataJobs, setDataJobs] = useState<any | null>(null);
   useEffect(() => {
     const fetchData = async () => {
@@ -36,78 +36,79 @@ export default function Jobs({ data }: any) {
         description={""}
         max_width
         height_auto
-        subtitle_max_width="max-w-[485px]"
+        subtitle_max_width='max-w-[485px]'
       />
-      <div className="container mx-auto pb-100">
-        <div className="flex justify-center gap-5">
-          <div className="flex-none">
+      <div className='container mx-auto px-2 pb-100'>
+        <div className='flex justify-center gap-5'>
+          <div className='flex-none w-[210px] md:w-auto'>
             <Image
               src={`${mediaUrl}${data.Image_01.data?.attributes.url}`}
-              alt=""
+              alt=''
               width={296}
               height={409}
             />
           </div>
-          <div className="flex-none">
+          <div className='flex-none hidden lg:block'>
             <Image
               src={`${mediaUrl}${data.Image_02.data?.attributes.url}`}
-              alt=""
+              alt=''
               width={667}
               height={409}
             />
           </div>
-          <div className="flex flex-none flex-col gap-5">
+          <div className='flex flex-none flex-col gap-5'>
             <Image
               src={`${mediaUrl}${data.Image_03.data?.attributes.url}`}
-              alt=""
+              alt=''
               width={206}
               height={258}
             />
             <Image
               src={`${mediaUrl}${data.Image_04.data?.attributes.url}`}
-              alt=""
+              alt=''
               width={206}
               height={146}
             />
           </div>
         </div>
         <h2
-          className="mt-100 text-center text-5xl font-semibold"
+          className='mt-100 text-center text-3xl lg:text-5xl font-semibold'
           dangerouslySetInnerHTML={{ __html: data.JoinTitle }}
         />
-        <div className="mt-60 grid gap-x-[25px] gap-y-[50px] md:grid-cols-3">
+        <div className='mt-60 grid gap-x-[25px] gap-y-[50px] px-3 lg:grid-cols-3'>
           {data.WhyJoin.map((item: any, index: number) => (
             <div key={index}>
-              <h4 className="mb-10 text-2xl font-bold">{item.Title}</h4>
-              <p className="text-l text-gray5">{item.Description}</p>
+              <h4 className='mb-10 text-xl md:text-2xl font-bold'>
+                {item.Title}
+              </h4>
+              <p className='text-l text-gray5'>{item.Description}</p>
             </div>
           ))}
         </div>
       </div>
 
-      <div className="bg-gold py-[150px] text-white">
-        <div className="">
-          <h2 className="mb-40 text-center text-5xl font-bold">
+      <div className='bg-gold  py-[80px] md:py-[150px] px-3 text-white'>
+        <div className=''>
+          <h2 className='mb-40 text-center text-3xl md:text-5xl font-bold'>
             {data.JobTitle}
           </h2>
-          <p className="mx-auto max-w-[825px] text-center text-l">
+          <p className='mx-auto max-w-[825px] text-center text-l'>
             {data.JobDescription}
           </p>
-          <ul className="mx-auto mt-[90px] max-w-6xl text-gray3">
+          <ul className='mx-auto lg:mt-[90px] max-w-6xl text-gray3'>
             {dataJobs?.map((item: any, index: number) => (
               <li key={index}>
                 <Link
                   href={`/job/${item.attributes.slug}`}
                   prefetch={false}
-                  className="flex items-center justify-between border-b pb-20 pt-[27px]"
-                >
-                  <div className="flex flex-col">
-                    <span className="text-[34px] font-bold">
+                  className='flex items-center justify-between border-b pb-20 pt-[27px]'>
+                  <div className='flex flex-col'>
+                    <span className='text-[34px] font-bold'>
                       {item.attributes.Name}
                     </span>
                     <span>{item.attributes.Location}</span>
                   </div>
-                  <span className="inline-block w-[26px]">
+                  <span className='inline-block w-[26px]'>
                     <LongArrow />
                   </span>
                 </Link>
@@ -117,45 +118,45 @@ export default function Jobs({ data }: any) {
         </div>
       </div>
 
-      <div className="px-4 pb-140 pt-[200px]">
-        <div className="container mx-auto">
-          <section className="text-center text-black">
+      <div className='px-4 py-[100px] lg:pb-140 lg:pt-[200px]'>
+        <div className='container mx-auto'>
+          <section className='text-center text-black'>
             <SectionTitles
               title={data.TestimonialsTitle}
               sub_title={data.TestimonialsDescription}
             />
           </section>
-          <div className="mt-[60px] grid grid-cols-3 gap-[60px]">
-            <div className="flex flex-col items-center justify-center bg-yellow2 px-[26px] py-50 text-center">
-              <Image src="/images/stars.webp" width={99} height={18} alt="" />
-              <p className="mb-10 mt-18 text-gray5">
+          <div className='mt-[60px] grid lg:grid-cols-3 gap-[60px]'>
+            <div className='flex flex-col items-center justify-center bg-yellow2 px-[26px] py-50 text-center'>
+              <Image src='/images/stars.webp' width={99} height={18} alt='' />
+              <p className='mb-10 mt-18 text-gray5'>
                 {
                   "Franchising with La Poire is not just a business venture; it's an immersion into Egypt's rich culinary culture. Become part of our growth story and carry "
                 }
               </p>
-              <span className="text-xs font-semibold text-gray">
+              <span className='text-xs font-semibold text-gray'>
                 Joe - UI & UX Designer
               </span>
             </div>
-            <div className="flex scale-110 flex-col items-center justify-center bg-yellow2 px-[26px] py-50 text-center">
-              <Image src="/images/stars.webp" width={99} height={18} alt="" />
-              <p className="mb-10 mt-18 text-gray5">
+            <div className='flex lg:scale-110 flex-col items-center justify-center bg-yellow2 px-[26px] py-50 text-center'>
+              <Image src='/images/stars.webp' width={99} height={18} alt='' />
+              <p className='mb-10 mt-18 text-gray5'>
                 {
                   "Franchising with La Poire is not just a business venture; it's an immersion into Egypt's rich culinary culture. Become part of our growth story and carry "
                 }
               </p>
-              <span className="text-xs font-semibold text-gray">
+              <span className='text-xs font-semibold text-gray'>
                 Joe - UI & UX Designer
               </span>
             </div>
-            <div className="flex flex-col items-center justify-center bg-yellow2 px-[26px] py-50 text-center">
-              <Image src="/images/stars.webp" width={99} height={18} alt="" />
-              <p className="mb-10 mt-18 text-gray5">
+            <div className='flex flex-col items-center justify-center bg-yellow2 px-[26px] py-50 text-center'>
+              <Image src='/images/stars.webp' width={99} height={18} alt='' />
+              <p className='mb-10 mt-18 text-gray5'>
                 {
                   "Franchising with La Poire is not just a business venture; it's an immersion into Egypt's rich culinary culture. Become part of our growth story and carry "
                 }
               </p>
-              <span className="text-xs font-semibold text-gray">
+              <span className='text-xs font-semibold text-gray'>
                 Joe - UI & UX Designer
               </span>
             </div>

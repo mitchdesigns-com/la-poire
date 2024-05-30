119af5523517bfbc8f11fdf71fba3616d53e03a0
BasmaElsisy <basmaelsisy@BasmaElsisys-MacBook-Pro.local>
2024-05-30 14:47:52 +0300

    updates by kareem

--- app/_components/Footer/Footer.tsx
+++ app/_components/Footer/Footer.tsx
@@ -4,28 +4,66 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="bg-black">
-      <div className="container mx-auto flex flex-col items-center justify-center gap-4 border-none pb-4 pt-4 text-center md:pt-64">
-        <Link href="/" className="my-20 inline-block md:my-0" prefetch={false}>
+    <footer className='bg-black'>
+      <div className='container mx-auto flex flex-col items-center gap-4 pb-4 pt-4 text-center md:pt-64'>
+        <Link href='/' className='my-4' prefetch={false}>
           <Image
-            src="/images/logo-white.webp"
-            alt="La Poire Logo"
-            width="166"
-            height="46"
+            src='/images/logo-white.webp'
+            alt='La Poire Logo'
+            width={166}
+            height={46}
           />
         </Link>
-        <span className="block h-[2px] w-full bg-gray5 opacity-50" />
-        <p className="px-2 text-sm tracking-normal text-gray5 opacity-50">
-          © {new Date().getFullYear()} La Poire Group • All Rights Reserved •{" "}
+        <div className='flex flex-wrap pl-2 justify-center gap-4 md:gap-8'>
           <Link
-            href="https://www.mitchdesigns.com/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Designed by MitchDesigns
+            href='/'
+            className='my-2 inline-block text-white'
+            prefetch={false}>
+            Homepage
           </Link>
-        </p>
+          <Link
+            href='/about'
+            className='my-2 inline-block text-white'
+            prefetch={false}>
+            About
+          </Link>
+          <Link
+            href='/brands'
+            className='my-2 inline-block text-white'
+            prefetch={false}>
+            Our Brands
+          </Link>
+          <Link
+            href='/franchise'
+            className='my-2 inline-block text-white'
+            prefetch={false}>
+            Franchise
+          </Link>
+          <Link
+            href='/job'
+            className='my-2 inline-block text-white'
+            prefetch={false}>
+            Careers
+          </Link>
+          <Link
+            href='/contact-us'
+            className='my-2 inline-block text-white'
+            prefetch={false}>
+            Contact us
+          </Link>
+        </div>
       </div>
-    </div>
+      <span className='block h-[2px] w-full  bg-gray5 opacity-50' />
+      <p className='flex flex-col md:flex-row pb-4 pt-4 px-2 text-sm tracking-normal items-center justify-center text-gray5 opacity-50'>
+        © {new Date().getFullYear()} La Poire Group • All Rights Reserved •{" "}
+        <Link
+          href='https://www.mitchdesigns.com/'
+          target='_blank'
+          rel='noreferrer'
+          className='flex  items-center'>
+          Designed by MitchDesigns
+        </Link>
+      </p>
+    </footer>
   );
 }
--- app/_components/UI/Questions.tsx
+++ app/_components/UI/Questions.tsx
@@ -8,20 +8,20 @@ export default function Questions({ data }: any) {
     setActiveIndex(activeIndex === index ? -1 : index);
   };
   return (
-    <div className="mx-auto flex w-[700px] max-w-full select-none flex-col gap-10">
+    <div className='mx-auto flex w-[700px] max-w-full select-none flex-col gap-10'>
       {data?.map((item: any, index: number) => (
         <div
-          className="flex cursor-pointer flex-col rounded-3xl border border-goldLight p-20 transition-all"
+          className='flex cursor-pointer flex-col rounded-3xl border border-goldLight p-20 transition-all'
           key={index}
-          onClick={() => toggleContent(index)}
-        >
-          <div className="flex items-center justify-between">
-            <h5 className="text-[15px] font-medium">{item.attributes.question}</h5>
+          onClick={() => toggleContent(index)}>
+          <div className='flex items-center gap-x-2  justify-between'>
+            <h5 className='text-[15px] font-medium'>
+              {item.attributes.question}
+            </h5>
             <span
               className={`text-gold w-32 h-32 bg-bej rounded-full flex justify-center items-center ${
                 activeIndex === index ? "rotate-180" : ""
-              }`}
-            >
+              }`}>
               <ChevronDown />
             </span>
           </div>

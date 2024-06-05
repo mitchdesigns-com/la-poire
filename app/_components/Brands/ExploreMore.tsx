87fc8170f1cbd63edf6b4fc42dae618665528c6b
kareem <kkareemtarek@gmail.com>
2024-06-05 18:07:16 +0300

    fix/ar/singlejob

--- app/_components/Brands/ExploreMore.tsx
+++ app/_components/Brands/ExploreMore.tsx
@@ -26,21 +26,20 @@ export default function ExploreMore({ parentSlug }: any) {
     (brand: any) => brand.attributes.slug !== parentSlug
   );
   return (
-    <div className="container mx-auto flex flex-col items-center justify-center gap-40 lg:py-70 pt-[42px]">
-      <h3 className="text-xl font-light">{t("explore_more_brands")}</h3>
-      <div className="flex flex-row items-center justify-center pl-4 pr-3 lg:gap-40">
+    <div className='container mx-auto flex flex-col items-center justify-center gap-40 lg:py-70 pt-[42px]'>
+      <h3 className='text-xl font-light'>{t("explore_more_brands")}</h3>
+      <div className='flex flex-row items-center justify-center  pr-3 lg:gap-40'>
         {Array.isArray(filteredBrands) &&
           filteredBrands.map((item: any, index: any) => (
             <Link
               href={`/brands/${item.attributes.slug}`}
               key={index}
-              className="relative inline-block h-[124px]  w-[130px] md:w-[142px]"
-            >
+              className='relative inline-block h-[124px]  w-[130px] md:w-[142px]'>
               <Image
-                alt="brand logo alt"
+                alt='brand logo alt'
                 fill
-                sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 33vw"
-                className="max-w-[120px] object-contain"
+                sizes='(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 33vw'
+                className='max-w-[120px] object-contain'
                 src={
                   process.env.NEXT_PUBLIC_API_URL +
                   item.attributes.Logo.data?.attributes.url

f6d3a65101a8e608d52c6e25bface8cf53b86cf9
BasmaElsisy <basmaelsisy@BasmaElsisys-MacBook-Pro.local>
2024-09-17 14:46:33 +0300

    add gtm

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,12 +1,13 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import Header from "@/app/_components/Header/Header";
-import { NextIntlClientProvider, createTranslator } from "next-intl";
-import { cls } from "@/app/utils/helpers";
-import { arfont, poppins } from "@/app/fonts";
+/* eslint-disable @next/next/inline-script-id */
 import Footer from "@/app/_components/Footer/Footer";
+import Header from "@/app/_components/Header/Header";
+import { arfont, poppins } from "@/app/fonts";
+import { cls } from "@/app/utils/helpers";
+import { NextIntlClientProvider } from "next-intl";
+import "./globals.css";
 // import { getMessages } from "next-intl/server";
 import { notFound } from "next/navigation";
+import Script from "next/script";
 
 // export const metadata: Metadata = {
 //   title: "La Poire",
@@ -71,7 +72,28 @@ export default async function RootLayout({
   const direction = locale === "en" ? "ltr" : "rtl";
   return (
     <html lang={locale} dir={direction}>
-      <body className={cls(`${locale === "ar"?arfont.className:poppins.className} bg-white`)}>
+      <head>
+        <Script
+          strategy="lazyOnload"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NVFS8L6B');
+            `,
+          }}
+        />
+      </head>
+      <body className={cls(`${locale === "ar" ? arfont.className : poppins.className} bg-white`)}>
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NVFS8L6B"
+            height="0" width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <NextIntlClientProvider locale={locale} messages={messages}>
           <div className="overflow-hidden">
             <Header />
